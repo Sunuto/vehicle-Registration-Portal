d563ef6962bb74171901f5ded70868d854f06c3a
Sunuto <sunutosharma104@gmail.com>
2026-05-18 21:41:56 +0545

    fixing the user authentication of the staff panel

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Profile
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_superuser']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile)
